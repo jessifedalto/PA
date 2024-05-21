826fbb21e4632753c622e39ddf4c2d338bfe9620
jessifedalto <142901908+jessifedalto@users.noreply.github.com>
2024-05-21 09:11:28 -0300

    Create primeiroCodigo.cs

---
+++ Aula1 /primeiroCodigo.cs
@@ -0,0 +1,39 @@
+using System;
+public class Program {
+    public static void Main (String[] args) {
+
+        Console.WriteLine ("Digite o valor para o primeiro número: ");
+
+        float num1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine ("\nDigite o valor para o segundo número: ");
+
+        float num2 = float.Parse(Console.ReadLine());
+
+        calculadora(num1, num2);
+    
+        Console.WriteLine("\nQual o valor do raio: ");
+        
+        float raio = float.Parse(Console.ReadLine());
+        
+        areaCirculo(raio);
+    }
+
+    static void calculadora(float num1, float num2) {
+
+        Console.WriteLine("A soma de " + num1 + " + " + num2 + " = " + (num1 + num2) + "\n");
+        
+        Console.WriteLine("A subtração de " + num1 + " - " + num2 + " = " + (num1 - num2) + "\n");
+        
+        Console.WriteLine("A multiplicação de " + num1 + " * " + num2 + " = " + (num1 * num2) + "\n");
+        
+        Console.WriteLine("A divisão de " + num1 + " / " + num2 + " = " + (num1 / num2) + "\n");
+    }
+
+    static void areaCirculo(float raio) {
+
+        double area = Math.PI * raio * raio;
+        
+        Console.WriteLine("\nA área do círculo de raio " + raio + " é igual a " + area.ToString("F2"));
+    }
+}
